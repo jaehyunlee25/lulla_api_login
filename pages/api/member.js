3f976a755457b2cafaa1bf0718fe6a89be53f52d
jaehyunlee <jaehyunlee25@gmail.com>
2021-08-06 23:29:14 +0900

    renew

---
+++ pages/api/member.js
@@ -0,0 +1,33 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+var {Client}=require("pg");
+var client=new Client({
+	user:"postgres",
+	host:"13.124.176.157",
+	database:"test",
+	password:"123456",
+	port:"5432"
+});
+export default aync function handler(req, res) {
+	var q1="select * from member;";
+	var data=await getData();
+	res.status(200).json(data)
+};
+function getData(){
+	client.connect();
+	var q1="select * from member;";
+	return new Promise((resolve,rej)=>{
+		client.query(
+			q1,
+			(err,res)=>{
+				if(err){
+					dir(err);
+					return;
+				}		
+				resolve(res);
+				client.end();
+			}
+		);		
+	});
+	
+};
+

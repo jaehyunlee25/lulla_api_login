c6e0a6f702af749c3edfaade7554570bc75a3113
jaehyunlee <jaehyunlee25@gmail.com>
2021-08-07 05:27:58 +0900

    renew

--- pages/api/member.js
+++ pages/api/member.js
@@ -1,34 +1,7 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-var {Client}=require("pg");
-var client=new Client({
-	user:process.env.db_user,
-	host:process.env.db_host,
-	database:process.env.db_name,
-	password:process.env.db_password,
-	port:process.env.db_port
-});
-console.dir(client);
+import getData from "/lib/pgConn"
 export default async function handler(req, res) {
-	var q1="select * from member;";
-	var data=await getData();
+	const q1="select * from member;";
+	let data=await getData(q1);
 	res.status(200).json(data);
 };
-function getData(){
-	client.connect();
-	var q1="select * from member;";
-	return new Promise((resolve,rej)=>{
-		client.query(
-			q1,
-			(err,res)=>{
-				if(err){
-					console.dir(err);
-					return;
-				}		
-				resolve(res);
-				client.end();
-			}
-		);		
-	});
-	
-};
-

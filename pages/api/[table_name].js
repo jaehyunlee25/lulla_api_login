4b4048a48e9d3cb2c1b38c7a95d97b043457fada
jaehyunlee <jaehyunlee25@gmail.com>
2021-08-09 17:02:27 +0900

    renew

--- pages/api/[table_name].js
+++ pages/api/[table_name].js
@@ -2,9 +2,9 @@
 import getData from "/lib/pgConn";
 import {useRouter} from "next/router";
 export default async function Handler(req,res){
-	const router=useRouter();
-	console.log(router);
-	const q1="select * from "+router.query.table_name+";";
+	//const router=useRouter();
+	//console.log(router);
+	const q1="select * from member;";
 	let data=await getData(q1);
 	res.status(200).json(data);
 };

b6d675ed722b3fc2f79fde50b77791b6c45dc4c9
jaehyunlee <jaehyunlee25@gmail.com>
2021-08-09 22:35:25 +0900

    renew

--- pages/api/[name].js
+++ pages/api/[name].js
@@ -4,8 +4,8 @@ import {useRouter} from "next/router";
 export default async function Handler(req,res){
 	//const router=useRouter();
 	//console.log(router);
-	console.log(req);
-	const q1="select * from member;";
+	console.log(req.query.name);
+	const q1="select * from "+req.query.name+";";
 	let data=await getData(q1);
 	res.status(200).json(data);
 };

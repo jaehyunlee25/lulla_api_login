ec83b35b1f72094496d1fbfb2214aa874864da1b
jaehyunlee <jaehyunlee25@gmail.com>
2021-08-09 21:30:55 +0900

    renew

--- pages/api/[table_name].js
+++ pages/api/[table_name].js
@@ -1,7 +1,7 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import getData from "/lib/pgConn";
 import {useRouter} from "next/router";
-export default async function Handler(req,res){
+export default async function handler(req,res){
 	//const router=useRouter();
 	//console.log(router);
 	const q1="select * from member;";

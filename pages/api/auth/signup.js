8f076b2cf5fa432a5267c8fc6576d337869c5cac
jaehyunlee <jaehyunlee25@gmail.com>
2021-08-10 20:47:20 +0900

    renew

--- pages/api/auth/signup.js
+++ pages/api/auth/signup.js
@@ -1,5 +1,9 @@
 import getData from "/lib/pgConn";
 export default async function handler(req,res){
+	if(req.method!="POST"){
+		res.status(200).json({result:"fail",reason:"not proper method"});
+		return;
+	}
 	const q1="select * from member;";
 	let data=await getData(q1);
 	res.status(200).json(data);

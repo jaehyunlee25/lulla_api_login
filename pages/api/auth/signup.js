ce8afc895bb4412f6bf3787baeb2ea522ba234ab
jaehyunlee <jaehyunlee25@gmail.com>
2021-08-12 00:33:33 +0900

    renew

--- pages/api/auth/signup.js
+++ pages/api/auth/signup.js
@@ -1,22 +1,14 @@
 import getData from "/lib/pgConn";
 export default async function handler(req,res){
 	//cors 해제
-	//console.log(res);
 	res.writeHead(200,{
 		"Access-Control-Allow-Origin":"*",
 		"Content-Type":"application/json",
 		"Access-Control-Allow-Headers":"*",
 		"Access-Control-Allow-Methods":"POST"
-	});
-	res.end("{'msg':'hello'}");
-	/* res.header("Access-Control-Allow-Origin", "*");
-	res.header("Access-Control-Allow-Headers", "X-Requested-With"); */
-	
-	/* if(req.method!="POST"){
-		res.status(200).json({result:"fail",reason:"not proper method"});
-		return;
-	} */
-	/* const q1="select * from member;";
+	});		
+	const q1="select * from member;";
 	let data=await getData(q1);
-	res.status(200).json(data); */
+	//res.status(200).json(data);
+	res.end(JSON.stringify(data));	
 };

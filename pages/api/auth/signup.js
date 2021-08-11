3ef78c33fe8aeab33c653743266cf78d4088756a
jaehyunlee <jaehyunlee25@gmail.com>
2021-08-12 00:41:33 +0900

    renew

--- pages/api/auth/signup.js
+++ pages/api/auth/signup.js
@@ -1,14 +1,21 @@
 import getData from "/lib/pgConn";
 export default async function handler(req,res){
-	//cors 해제
+	//회원가입
+	
+	//#1. cors 해제
 	res.writeHead(200,{
 		"Access-Control-Allow-Origin":"*",
 		"Content-Type":"application/json",
 		"Access-Control-Allow-Headers":"*",
 		"Access-Control-Allow-Methods":"POST"
-	});		
-	const q1="select * from member;";
-	let data=await getData(q1);
-	//res.status(200).json(data);
+	});
+	
+	//#2. operation
+	/* const q1="select * from member;";
+	let data=await getData(q1); */
+	console.log(req.body);
+	
+	
+	//#3. data return
 	res.end(JSON.stringify(data));	
 };

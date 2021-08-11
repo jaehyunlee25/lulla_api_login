359e75edd222f9ab0470856280481294ae6cbd77
jaehyunlee <jaehyunlee25@gmail.com>
2021-08-12 01:57:21 +0900

    renew

--- pages/api/auth/signup.js
+++ pages/api/auth/signup.js
@@ -13,15 +13,13 @@ export default async function handler(req,res){
 	//#2. operation
 	/* const q1="select * from member;";
 	let data=await getData(q1); */
-	
-	
-	var data=req.body,
-		user_info=JSON.parse(data).user_info,
-		temporary;
-	
-	var result=await getData("select * from users where phone='"+user_info.phone+"' and activated=false;");
-	
-	//#3. data return
-	res.end(JSON.stringify(result));		
-	
+	if(req.body.length>0){
+		var data=req.body,
+			user_info=JSON.parse(data).user_info,
+			temporary;
+		var result=await getData("select * from users where phone='"+user_info.phone+"' and activated=false;");
+		
+		//#3. data return
+		res.end(JSON.stringify(result));		
+	}
 };

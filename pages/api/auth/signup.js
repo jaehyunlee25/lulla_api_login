dd327056c022362fed548835b146ff5eb039919d
jaehyunlee <jaehyunlee25@gmail.com>
2021-08-13 06:14:32 +0900

    renew

--- pages/api/auth/signup.js
+++ pages/api/auth/signup.js
@@ -1,5 +1,5 @@
 import {RESPOND,ERROR,PASSWORD,TOKEN} from "/lib/apiCommon";	//include String.prototype.fQuery
-import procQuery from "/lib/pgConn";	//include String.prototype.fQuery
+import setBaseURL from "/lib/pgConn";	//include String.prototype.fQuery
 
 export default async function handler(req,res){
 	//회원가입	
@@ -19,6 +19,7 @@ export default async function handler(req,res){
 	//#3.1. 비밀번호 처리 우선
 	req.body.user_info.password=await PASSWORD(req.body.user_info.password);
 	//#3.2. 작업
+	setBaseURL("sqls/auth/signup");	//끝에 슬래시 붙이지 마시오.
 	var USER,
 		QTS={	//Query TemplateS
 			getWasUsers:"getWasUsers",
@@ -38,7 +39,6 @@ export default async function handler(req,res){
 	//#3.2.2. 기존의 번호가 있으면, 탈퇴한 번호를 재활용한다.
 	if(wasUsers.length>0){
 		var wasUser=wasUsers[0];
-		
 		//#3.2.2.1 탈퇴한 정보를 활성화하고, 새로운 정보로 수정한다(기존 정보를 재활용하지만, 기존 사용자임을 보장하진 않는다).	
 		var	qSetUser=await QTS.setUser.fQuery({
 				activated:true,
@@ -50,26 +50,26 @@ export default async function handler(req,res){
 		if(qSetUser.type=="error") 
 			return ERROR(res,{id:"ERR.auth.signup.2",message:"user update failed"});
 		
-		//#3.2.2.2 활성화한 사용자의 정보를 추출한다.
-		var qUser=await QTS.getUserById.fQuery({id:wasUser.id});
-		if(qUser.type=="error") 
-			return ERROR(res,{id:"ERR.auth.signup.3",message:"user not found after user update"});
-		USER=qUser.message.rows[0];
-		
-		//#3.2.2.3 활성화한 사용자의 정보를 바탕으로 관련된 학원 인원 명단을 추출한다.
-		var qSchoolMembers=await QTS.getSchoolMember.fQuery({user_id:USER.id});			
-		if(qSchoolMembers.type=="error") 
-			return ERROR(res,{id:"ERR.auth.signup.4",message:"schoolMembers query failed"});
-		var	schoolMembers=qSchoolMembers.message.rows;
-		
-		//#3.2.2.4 활성화한 사용자의 정보를 바탕으로 타임아웃 토큰을 발행한다.
-		var token=TOKEN(USER);
-			
-		//#3.2.2.5 활성화한 사용자, 토큰, 학원인원을 리턴한다.
-		return RESPOND(res,{token,USER,schoolMembers});
-		
+	//#3.2.3. 기존의 번호가 없으면, 새로 등록한다.
 	}else{	//신규회원 창설
 		return RESPOND(res,{type:"empty"});
 	}
-	return RESPOND(res,{type:"end"});
+	
+	//#3.2.2.2 활성화한 사용자의 정보를 추출한다.
+	var qUser=await QTS.getUserById.fQuery({id:wasUser.id});
+	if(qUser.type=="error") 
+		return ERROR(res,{id:"ERR.auth.signup.3",message:"user not found after user update"});
+	USER=qUser.message.rows[0];
+	
+	//#3.2.2.3 활성화한 사용자의 정보를 바탕으로 관련된 학원 인원 명단을 추출한다.
+	var qSchoolMembers=await QTS.getSchoolMember.fQuery({user_id:USER.id});			
+	if(qSchoolMembers.type=="error") 
+		return ERROR(res,{id:"ERR.auth.signup.4",message:"schoolMembers query failed"});
+	var	schoolMembers=qSchoolMembers.message.rows;
+	
+	//#3.2.2.4 활성화한 사용자의 정보를 바탕으로 타임아웃 토큰을 발행한다.
+	var token=TOKEN(USER);
+		
+	//#3.2.2.5 활성화한 사용자, 토큰, 학원인원을 리턴한다.
+	return RESPOND(res,{token,USER,schoolMembers});
 };

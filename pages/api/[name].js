0e7d89d1ce9ad5dbb73993141bc3ea04e06d51ac
jaehyunlee <jaehyunlee25@gmail.com>
2021-08-13 17:04:12 +0900

    renew

--- pages/api/[name].js
+++ pages/api/[name].js
@@ -3,6 +3,6 @@ import {RESPOND,ERROR} from "/lib/apiCommon";
 import setBaseURL from "/lib/pgConn";	//include String.prototype.fQuery
 export default async function handler(req,res){
 	setBaseURL("sqls");	//끝에 슬래시 붙이지 마시오!
-	var qName="name".fQuery({table_name:"users"});
+	var qName="name".fQuery({table_name:req.query.name});
 	res.status(200).json(qName);
 };

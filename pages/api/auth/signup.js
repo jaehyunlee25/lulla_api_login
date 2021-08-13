3810053617f9678bb032daba4c0e6ebb4baaac83
jaehyunlee <jaehyunlee25@gmail.com>
2021-08-13 09:20:26 +0900

    renew

--- pages/api/auth/signup.js
+++ pages/api/auth/signup.js
@@ -32,55 +32,67 @@ export default async function handler(req,res){
 			newUser:"newUser",
 		},
 		data=req.body,
-		user_info=data.user_info,
+		user_info=data.user_info;
 		
 	//#3.2.1. 전화번호를 바탕으로 기존 사용자가 있는지 찾아본다.
-		qUsers=await QTS.getWasUsers.fQuery({phone:user_info.phone,activated:false});	
-	if(qUsers.type=="error") 
+	try{
+		var	qUsers=await QTS.getWasUsers.fQuery({phone:user_info.phone,activated:false});	
+	}catch(e){
 		return ERROR(res,{id:"ERR.auth.signup.1",message:"user not found"});
+	}
 	var	wasUsers=qUsers.message.rows;
 		
 	//#3.2.2. 기존의 번호가 있으면, 탈퇴한 번호를 재활용한다.
 	if(wasUsers.length>0){
 		var wasUser=wasUsers[0];
-		//#3.2.2.1 탈퇴한 정보를 활성화하고, 새로운 정보로 수정한다(기존 정보를 재활용하지만, 기존 사용자임을 보장하진 않는다).	
-		var	qSetUser=await QTS.setUser.fQuery({
-				activated:true,
-				password:user_info.password,
-				email:user_info.email,
-				name:user_info.name,
-				id:wasUser.id
-			});
-		if(qSetUser.type=="error") 
+		//#3.2.2.1 탈퇴한 정보를 활성화하고, 새로운 정보로 수정한다(기존 정보를 재활용하지만, 기존 사용자임을 보장하진 않는다).
+		try{
+			var	qSetUser=await QTS.setUser.fQuery({
+					activated:true,
+					password:user_info.password,
+					email:user_info.email,
+					name:user_info.name,
+					id:wasUser.id
+				});
+		}catch(e){
 			return ERROR(res,{id:"ERR.auth.signup.2",message:"user update failed"});
+		}
 		
 		//#3.2.2.2 활성화한 사용자의 정보를 추출한다.
-		var qUser=await QTS.getUserById.fQuery({id:wasUser.id});
-		if(qUser.type=="error") 
+		try{
+			var qUser=await QTS.getUserById.fQuery({id:wasUser.id});
+		}catch(e){
 			return ERROR(res,{id:"ERR.auth.signup.4",message:"user not found after user update"});
+		}
 		USER=qUser.message.rows[0];
 	
 	//#3.2.3. 기존의 번호가 없으면, 새로 등록한다.
 	}else{	//신규회원 창설
 		//주로 정보의 중복이나 검증에 관한 작업이다.
 		//#3.2.3.1 이메일 중복 체크
-		var qSEs=await QTS.getSameEmails.fQuery({email:user_info.email});
-		if(qSEs.type=="error") 
+		try{
+			var qSEs=await QTS.getSameEmails.fQuery({email:user_info.email});
+		}catch(e){
 			return ERROR(res,{id:"ERR.auth.signup.3.2.3.1.1",message:"email query failed"});
+		}
 		if(qSEs.message.rows.length>0)
 			return ERROR(res,{id:"ERR.auth.signup.3.2.3.1.2",message:"same email existing"});
 		
 		//#3.2.3.2 전화번호 중복 체크
-		var qSPs=await QTS.getSamePhones.fQuery({phone:user_info.phone});
-		if(qSPs.type=="error") 
+		try{
+			var qSPs=await QTS.getSamePhones.fQuery({phone:user_info.phone});
+		}catch(e){
 			return ERROR(res,{id:"ERR.auth.signup.3.2.3.2.1",message:"phone query failed"});
+		}
 		if(qSPs.message.rows.length>0)
 			return ERROR(res,{id:"ERR.auth.signup.3.2.3.2.2",message:"same phone existing"});
 		
 		//#3.2.3.3 전화번호 인증 체크
-		var qVN=await QTS.getVerifyNumber.fQuery({phone:user_info.phone});
-		if(qVN.type=="error") 
+		try{
+			var qVN=await QTS.getVerifyNumber.fQuery({phone:user_info.phone});
+		}catch(e){
 			return ERROR(res,{id:"ERR.auth.signup.3.2.3.3.1",message:"phone verify query failed"});
+		}
 		if(qVN.message.rows.length==0)
 			return ERROR(res,{id:"ERR.auth.signup.3.2.3.3.2",message:"not verified number"});
 		

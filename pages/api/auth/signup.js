1f14c8fa5f0ad00d22763e4cd5a70684167d3960
jaehyunlee <jaehyunlee25@gmail.com>
2021-08-17 20:00:44 +0900

    'renew'

--- pages/api/auth/signup.js
+++ pages/api/auth/signup.js
@@ -120,7 +120,7 @@ export default async function handler(req, res) {
 
   if (data.type === 'local') {
     const result = await procLocal(res, data);
-    if (result === 'error' ) return false;
+    if (result === 'error') return false;
   } else {
     procSocial(res, data);
   }

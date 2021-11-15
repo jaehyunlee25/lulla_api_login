3b69bd0975017aec68f8554112a89bf0433b8fc7
jaehyunlee <jaehyunlee25@gmail.com>
2021-11-16 04:51:54 +0900

    renew

--- sqls/auth/signin/getInvitation.sql
+++ sqls/auth/signin/getInvitation.sql
@@ -1,11 +1,14 @@
 select 
     i.id,
-	s.name,
-	i.role_name,
+	u.name user_name,
+	s.name school_name,
+	i.role_name role_name,
+	i.kid_name kid_name,
 	to_char(i.created_at, 'YYYY.MM.DD') date
 from 
     invitation i
 	left join schools s on s.id = i.school_id
+    left join users u on u.id = i.user_id
 where
     user_id = '${userId}'
     and confirmed = false
